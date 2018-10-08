c9f6f3f826db1a1dee1d916362121f645b8eeff5
zhouXianSheng11 <694506680@qq.com>
2018-10-08 17:40:13 +0800

    develop - version

--- src/com/pro/demo/Test_one.java
+++ src/com/pro/demo/Test_one.java
@@ -4,7 +4,7 @@ public class Test_one {
     public static void main(String[] args) {
         System.out.print("版本一");
 
-	System.out.print("master---------支建立---------------");
+	System.out.print("develop---------支建立---------------");
 
 	System.out.print("提交二");
 
